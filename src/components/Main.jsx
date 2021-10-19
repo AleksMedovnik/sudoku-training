fe06ba01f58067427245dbb4192770e6c6c4d738
Aleksandr <medovnikaleksander@yandex.ua>
2021-10-19 16:12:40 +0300

    Added reducer & useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const Main = (props) => {
+
+    const [indexArr, newIndexArr] = useState([0, 0]);
+
+    const setActiveInput = (a, b) => {
+        newIndexArr([a, b]);
+        // console.log(indexArr) - useState асинхронный
+    }
+
     const tbody = props.matrix.map((tr, trIndex) => {
         return (
             <tr key={`_${trIndex}`}>{tr.map((td, index) => {
                 return (
-                    <td key={`_${trIndex}_${index}`}>
+                    <td key={`_${trIndex}_${index}`} onClick={() => setActiveInput(trIndex, index)}  className={(trIndex === indexArr[0] && index === indexArr[1]) ? 'td active-cell' : 'td'}>
                         {td || ''}
                     </td>
                 )
@@ -11,6 +21,7 @@ const Main = (props) => {
             </tr>
         )
     });
+
     const values = props.valuesData.map(i => <li key={i} data-value={i}>{i}</li>);
     
 
