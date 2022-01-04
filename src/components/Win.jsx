2ab4b4a17f60672bf871ccb0c0200bb03d217d85
Aleksandr <medovnikaleksander@yandex.ua>
2022-01-04 11:38:08 +0300

    Update react-router-dom

--- src/components/Win.jsx
+++ src/components/Win.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Win = () => {
     return (
         <div className='container'>
-            <h1 className='title'>You Win!!!</h1>
+            <h1 className='title'>You Won!!!</h1>
             <NavLink to='/main' className='link-start'>Play Again</NavLink>
         </div>
     );
